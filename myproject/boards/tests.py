30f75aa101080a5388fafd4d36651d0ff906b9ea
shunsuke-t <st.0316@gmail.com>
2018-09-15 13:58:09 +0900

    #17 is done.

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -2,8 +2,9 @@
 # to change below ar ver2.0
 from django.urls import reverse, resolve
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .views import home, board_topics, new_topic
-from .models import Board
+from .models import Board, Topic, Post
 
 
 class HomeTests(TestCase):
@@ -59,6 +60,9 @@ class BoardTopicsTests(TestCase):
 class NewTopicTests(TestCase):
     def setUp(self):
         Board.objects.create(name='Django', description='Django board.')
+        User.objects.create_user(
+            username='jhon', email='jhon@doe.com', password='123'
+        )  # <- included this line here
 
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
@@ -79,3 +83,42 @@ class NewTopicTests(TestCase):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(new_topic_url)
         self.assertContains(response, 'href="{0}"'.format(board_topics_url))
+
+    def test_csrf(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_topic_vaild_post_date(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': 'Test title',
+            'message': 'Lorem ipsum dolor sit amet'
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    def test_new_topic_invaild_post_date(self):
+        '''
+        invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_invaild_post_date_empty_fields(self):
+        '''
+        Invalid post data should not redirect
+        The expected bhavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': '',
+            'message': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post.objects.exists())
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
+from .forms import NewTopicForm
 # from django.http import Http404
 from .models import Board, Topic, Post
 
@@ -21,26 +22,22 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-
+    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            # TODO: redirect to the created topic page
+            return redirect('board_topics', pk=board.pk)
 
-        user = User.objects.first()  # TODO: get the currently logged in user
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        # TODO: redirect to the created topic page
-        return redirect('board_topics', pk=board.pk)
-
-    return render(request, 'new_topic.html', {'board': board})
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
