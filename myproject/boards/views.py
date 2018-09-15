22aa997cd9ff78fd7e5bf6516dcfdfa3ed7062ce
shunsuke-t <st.0316@gmail.com>
2018-09-15 10:10:51 +0900

    second commit

---
+++ myproject/boards/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+# from django.http import Http404
+from .models import Board
+
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    # try:
+    #     board = Board.objects.get(pk=pk)
+    # except Board.DoesNotExist:
+    #     raise Http404
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
