dbd6a3f9916b0227b32d14229a62b34da18b8e91
shunsuke-t <st.0316@gmail.com>
2018-09-23 00:17:01 +0900

    PRCTests is done

--- myproject/accounts/tests/test_view_password_rest.py
+++ myproject/accounts/tests/test_view_password_rest.py
@@ -34,3 +34,80 @@ class PasswordResetTests(TestCase):
         '''
         self.assertContains(self.response, '<input', 2)
         self.assertContains(self.response, 'type="email"', 1)
+
+
+class SuccessfulPasswordResetTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset')
+        self.response = self.client.post(
+            url, {'email': 'do_not_exist@email.com'})
+
+    def test_redirection(self):
+        '''
+        Even invalid emails in the database should
+        redirect the user to `password_reset_done` view.
+        '''
+        url = reverse('password_reset_done')
+        self.assertRedirects(self.response, url)
+
+    def test_no_reset_email_sent(self):
+        self.assertEqual(0, len(mail.outbox))
+
+
+class PasswordResetDoneTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset_done')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/done/')
+        self.assertEquals(view.func.view_class,
+                          auth_views.PasswordResetDoneView)
+
+
+class PasswordResetConfirmTests(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(
+            username='john',
+            email='john@doe.com',
+            password='123abcdef'
+        )
+        '''
+        create a valid password reset token
+        based on how django creates the token internally;
+        https://github.com/django/django/blob/1.11.5/django/contrib/auth/forms.py#L280
+        '''
+        self.uid = urlsafe_base64_encode(force_bytes(user.pk)).decode()
+        self.token = default_token_generator.make_token(user)
+
+        url = reverse('password_reset_confirm', kwargs={
+            'uidb64': self.uid,
+            'token': self.token
+        })
+        self.response = self.client.get(url, follow=True)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve(
+            '/reset/{uidb64}/{token}/'.format(uidb64=self.uid, token=self.token))
+        self.assertEquals(view.func.view_class,
+                          auth_views.PasswordResetConfirmView)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, SetPasswordForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain two inputs: csrf and two passwords fields.
+        '''
+        self.assertContains(self.response, '<input', 3)
+        self.assertContains(self.response, 'type="password"', 2)
