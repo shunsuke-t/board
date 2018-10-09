dad0bb739f48b7841e1c672be43bcc07fef0a989
shunsuke-t <st.0316@gmail.com>
2018-10-09 20:47:12 +0900

    '#32 is done'

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.html import mark_safe
 from django.utils.text import Truncator
 from django.contrib.auth.models import User
+from markdown import markdown
 
 
 class Board(models.Model):
@@ -44,3 +46,6 @@ class Post(models.Model):
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
